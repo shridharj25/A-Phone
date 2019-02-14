365e02e2e7fca539de9c687ca4ad8850c1dd84f8
p17178463-Shridhar <p17178463@my365.dmu.ac.uk>
2019-02-14 14:33:01 +0000

    Added Order Tests

---
+++ APhoneLibrary/clsOrder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace APhoneLibrary
+{
+    public class clsOrder
+    {
+        public DateTime OrderDate { get; set; }
+        public double Price { get; set; }
+        public string OrderMadeBy { get; set; }
+        public DateTime OrderDueDate { get; set; }
+    }
+}
--- APhoneTestProject/APhoneTests.cs
+++ APhoneTestProject/APhoneTests.cs
@@ -8,8 +8,8 @@ namespace APhoneTestProject
     [TestClass]
     public class APhoneTests
     {
-        [TestMethod]
-        public void InstanceOK()
+       [TestMethod]
+        public void PhoneInstanceOK()
         {
             //create an instance of the class
             clsPhone APhone = new clsPhone();
@@ -94,20 +94,70 @@ namespace APhoneTestProject
             //test to see that the two values are the same
             Assert.AreEqual(APhone.CameraQuality, someCameraQuality);
         }
+        //End of Lewis' Phone tests
 
+        //Start of Shridhar's Tests
         [TestMethod]
-        public void PhoneIdOK()
+        public void OrderInstanceOK()
         {
             //create an instance of the class
-            clsPhone APhone = new clsPhone();
-            //create some test data 
-            int somePhoneId = 1;
-            //assign the data to property
-            APhone.PhoneId = somePhoneId;
-            //test to see that the two values are the same
-            Assert.AreEqual(APhone.PhoneId, somePhoneId);
+            clsOrder AOrder = new clsOrder();
+            //test to see that it exists
+            Assert.IsNotNull(AOrder);
         }
-        //End of Lewis' Phone tests
+
+        [TestMethod]
+        public void OrderDateOK()
+        {
+            //create instance of the class
+            clsOrder AOrder = new clsOrder();
+            //create some test data 
+            DateTime someOrderDate = Convert.ToDateTime("01/01/2011");
+            //assign the data property
+            AOrder.OrderDate = someOrderDate;
+            //test to see the that the two values are the same
+            Assert.AreEqual(AOrder.OrderDate, someOrderDate);
+        }
+
+        [TestMethod]
+        public void OrderPriceOK()
+        {
+            //create an instance of the class
+            clsOrder AOrder = new clsOrder();
+            //create some test data 
+            double somePrice = 450.0;
+            //assign the data to property
+            AOrder.Price = somePrice;
+            //test to see that the two values are the same
+            Assert.AreEqual(AOrder.Price, somePrice);
+        }
+
+        [TestMethod]
+        public void PhoneOrderByOK()
+        {
+            //create an instance of the class
+            clsOrder AOrder = new clsOrder();
+            //create some test data 
+            string someOrderMadeby = "John";
+            //assign the data to property
+            AOrder.OrderMadeBy = someOrderMadeby;
+            //test to see that the two values are the same
+            Assert.AreEqual(AOrder.OrderMadeBy, someOrderMadeby);
+        }
+
+        [TestMethod]
+        public void OrderDueDateOK()
+        {
+            //create instance of the class
+            clsOrder AOrder = new clsOrder();
+            //create some test data 
+            DateTime someOrderDueDate = Convert.ToDateTime("01/01/2011");
+            //assign the date to property
+            AOrder.OrderDueDate = someOrderDueDate;
+            //test to check that the two values are equal
+            Assert.AreEqual(AOrder.OrderDueDate, someOrderDueDate);
+        }
+        //End of Shridhar's Tests
     }
 }
 
