1f5040e09743a63579bd841f828e914f39b110b3
Connor19 <connor.reid98@btinternet.com>
2019-02-20 10:47:14 +0000

    Added Tariff Class test Methods

---
+++ APhoneLibrary/clsTariff.cs
@@ -0,0 +1,13 @@
+﻿namespace APhoneLibrary
+{
+    public class clsTariff
+    {
+        public string Calls { get; set; }
+        public string Data { get; set; }
+        public int TariffID { get; set; }
+        public string Network { get; set; }
+        public double Price { get; set; }
+        public string Texts { get; set; }
+        public double Upfront { get; set; }
+    }
+}
