609b68f5696e0a001d89d32fe0fe5cccb622e911
p17178463-Shridhar <p17178463@my365.dmu.ac.uk>
2019-02-21 14:24:36 +0000

    Fixed everything

--- APhoneTestProject/tstOrder.cs
+++ APhoneTestProject/tstOrder.cs
@@ -1,62 +1,73 @@
-﻿//Start of Shridhar's Tests
-[TestMethod]
-public void OrderInstanceOK()
-{
-    //create an instance of the class
-    clsOrder AOrder = new clsOrder();
-    //test to see that it exists
-    Assert.IsNotNull(AOrder);
-}
+﻿using System;
+using APhoneLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-[TestMethod]
-public void OrderDateOK()
+namespace OrderTestProject
 {
-    //create instance of the class
-    clsOrder AOrder = new clsOrder();
-    //create some test data 
-    DateTime someOrderDate = Convert.ToDateTime("01/01/2011");
-    //assign the data property
-    AOrder.OrderDate = someOrderDate;
-    //test to see the that the two values are the same
-    Assert.AreEqual(AOrder.OrderDate, someOrderDate);
-}
+    [TestClass]
+    public class OrderTests
+    {
+        //Start of Shridhar's Tests
+        [TestMethod]
+        public void OrderInstanceOK()
+        {
+            //create an instance of the class
+            clsOrder AOrder = new clsOrder();
+            //test to see that it exists
+            Assert.IsNotNull(AOrder);
+        }
 
-[TestMethod]
-public void OrderPriceOK()
-{
-    //create an instance of the class
-    clsOrder AOrder = new clsOrder();
-    //create some test data 
-    double somePrice = 450.0;
-    //assign the data to property
-    AOrder.Price = somePrice;
-    //test to see that the two values are the same
-    Assert.AreEqual(AOrder.Price, somePrice);
-}
+        [TestMethod]
+        public void OrderDateOK()
+        {
+            //create instance of the class
+            clsOrder AOrder = new clsOrder();
+            //create some test data 
+            DateTime someOrderDate = Convert.ToDateTime("01/01/2011");
+            //assign the data property
+            AOrder.OrderDate = someOrderDate;
+            //test to see the that the two values are the same
+            Assert.AreEqual(AOrder.OrderDate, someOrderDate);
+        }
 
-[TestMethod]
-public void PhoneOrderByOK()
-{
-    //create an instance of the class
-    clsOrder AOrder = new clsOrder();
-    //create some test data 
-    string someOrderMadeby = "John";
-    //assign the data to property
-    AOrder.OrderMadeBy = someOrderMadeby;
-    //test to see that the two values are the same
-    Assert.AreEqual(AOrder.OrderMadeBy, someOrderMadeby);
-}
+        [TestMethod]
+        public void OrderPriceOK()
+        {
+            //create an instance of the class
+            clsOrder AOrder = new clsOrder();
+            //create some test data 
+            double somePrice = 450.0;
+            //assign the data to property
+            AOrder.Price = somePrice;
+            //test to see that the two values are the same
+            Assert.AreEqual(AOrder.Price, somePrice);
+        }
 
-[TestMethod]
-public void OrderDueDateOK()
-{
-    //create instance of the class
-    clsOrder AOrder = new clsOrder();
-    //create some test data 
-    DateTime someOrderDueDate = Convert.ToDateTime("01/01/2011");
-    //assign the date to property
-    AOrder.OrderDueDate = someOrderDueDate;
-    //test to check that the two values are equal
-    Assert.AreEqual(AOrder.OrderDueDate, someOrderDueDate);
+        [TestMethod]
+        public void PhoneOrderByOK()
+        {
+            //create an instance of the class
+            clsOrder AOrder = new clsOrder();
+            //create some test data 
+            string someOrderMadeby = "John";
+            //assign the data to property
+            AOrder.OrderMadeBy = someOrderMadeby;
+            //test to see that the two values are the same
+            Assert.AreEqual(AOrder.OrderMadeBy, someOrderMadeby);
+        }
+
+        [TestMethod]
+        public void OrderDueDateOK()
+        {
+            //create instance of the class
+            clsOrder AOrder = new clsOrder();
+            //create some test data 
+            DateTime someOrderDueDate = Convert.ToDateTime("01/01/2011");
+            //assign the date to property
+            AOrder.OrderDueDate = someOrderDueDate;
+            //test to check that the two values are equal
+            Assert.AreEqual(AOrder.OrderDueDate, someOrderDueDate);
+        }
+        //End of Shridhar's Tests
+    }
 }
-//End of Shridhar's Tests
--- APhoneTestProject/tstTariff.cs
+++ APhoneTestProject/tstTariff.cs
@@ -1,101 +1,112 @@
-﻿//Start Of Connor's Tests
-[TestMethod]
-public void TariffInstanceOK()
-{
-    //create an instance of the class
-    clsTariff ATariff = new clsTariff();
-    //test to see that it exists
-    Assert.IsNotNull(ATariff);
-}
+﻿using System;
+using APhoneLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-[TestMethod]
-public void TariffCallsOK()
+namespace TariffTestProject
 {
-    //create an instance of the class
-    clsTariff ATariff = new clsTariff();
-    //create some test data 
-    string callLimit = "Unlimited";
-    //assign the data to property
-    ATariff.Calls = callLimit;
-    //test to see that the two values are the same
-    Assert.AreEqual(ATariff.Calls, callLimit);
-}
+    [TestClass]
+    public class TariffTests
+    {
+        //Start Of Connor's Tests
+        [TestMethod]
+        public void TariffInstanceOK()
+        {
+            //create an instance of the class
+            clsTariff ATariff = new clsTariff();
+            //test to see that it exists
+            Assert.IsNotNull(ATariff);
+        }
 
-[TestMethod]
-public void TariffDataOK()
-{
-    //create an instance of the class
-    clsTariff ATariff = new clsTariff();
-    //create some test data 
-    string dataLimit = "20GB";
-    //assign the data to property
-    ATariff.Data = dataLimit;
-    //test to see that the two values are the same
-    Assert.AreEqual(ATariff.Data, dataLimit);
-}
+        [TestMethod]
+        public void TariffCallsOK()
+        {
+            //create an instance of the class
+            clsTariff ATariff = new clsTariff();
+            //create some test data 
+            string callLimit = "Unlimited";
+            //assign the data to property
+            ATariff.Calls = callLimit;
+            //test to see that the two values are the same
+            Assert.AreEqual(ATariff.Calls, callLimit);
+        }
 
-[TestMethod]
-public void TariffIDOK()
-{
-    //create an instance of the class
-    clsTariff ATariff = new clsTariff();
-    //create some test data 
-    Int32 TariffID = 1;
-    //assign the data to property
-    ATariff.TariffID = TariffID;
-    //test to see that the two values are the same
-    Assert.AreEqual(ATariff.TariffID, TariffID);
-}
+        [TestMethod]
+        public void TariffDataOK()
+        {
+            //create an instance of the class
+            clsTariff ATariff = new clsTariff();
+            //create some test data 
+            string dataLimit = "20GB";
+            //assign the data to property
+            ATariff.Data = dataLimit;
+            //test to see that the two values are the same
+            Assert.AreEqual(ATariff.Data, dataLimit);
+        }
 
-[TestMethod]
-public void TariffNetworkOK()
-{
-    //create an instance of the class
-    clsTariff ATariff = new clsTariff();
-    //create some test data 
-    string tariffNetwork = "O2";
-    //assign the data to property
-    ATariff.Network = tariffNetwork;
-    //test to see that the two values are the same
-    Assert.AreEqual(ATariff.Network, tariffNetwork);
-}
+        [TestMethod]
+        public void TariffIDOK()
+        {
+            //create an instance of the class
+            clsTariff ATariff = new clsTariff();
+            //create some test data 
+            Int32 TariffID = 1;
+            //assign the data to property
+            ATariff.TariffID = TariffID;
+            //test to see that the two values are the same
+            Assert.AreEqual(ATariff.TariffID, TariffID);
+        }
 
-[TestMethod]
-public void TariffPriceOK()
-{
-    //create an instance of the class
-    clsTariff ATariff = new clsTariff();
-    //create some test data 
-    double tariffPrice = 45.00;
-    //assign the data to property
-    ATariff.Price = tariffPrice;
-    //test to see that the two values are the same
-    Assert.AreEqual(ATariff.Price, tariffPrice);
-}
+        [TestMethod]
+        public void TariffNetworkOK()
+        {
+            //create an instance of the class
+            clsTariff ATariff = new clsTariff();
+            //create some test data 
+            string tariffNetwork = "O2";
+            //assign the data to property
+            ATariff.Network = tariffNetwork;
+            //test to see that the two values are the same
+            Assert.AreEqual(ATariff.Network, tariffNetwork);
+        }
 
-[TestMethod]
-public void TariffTextsOK()
-{
-    //create an instance of the class
-    clsTariff ATariff = new clsTariff();
-    //create some test data 
-    string tariffTexts = "Unlimited";
-    //assign the data to property
-    ATariff.Texts = tariffTexts;
-    //test to see that the two values are the same
-    Assert.AreEqual(ATariff.Texts, tariffTexts);
-}
+        [TestMethod]
+        public void TariffPriceOK()
+        {
+            //create an instance of the class
+            clsTariff ATariff = new clsTariff();
+            //create some test data 
+            double tariffPrice = 45.00;
+            //assign the data to property
+            ATariff.Price = tariffPrice;
+            //test to see that the two values are the same
+            Assert.AreEqual(ATariff.Price, tariffPrice);
+        }
 
-[TestMethod]
-public void TariffUpfrontOK()
-{
-    //create an instance of the class
-    clsTariff ATariff = new clsTariff();
-    //create some test data 
-    double tariffUpfront = 90.00;
-    //assign the data to property
-    ATariff.Upfront = tariffUpfront;
-    //test to see that the two values are the same
-    Assert.AreEqual(ATariff.Upfront, tariffUpfront);
+        [TestMethod]
+        public void TariffTextsOK()
+        {
+            //create an instance of the class
+            clsTariff ATariff = new clsTariff();
+            //create some test data 
+            string tariffTexts = "Unlimited";
+            //assign the data to property
+            ATariff.Texts = tariffTexts;
+            //test to see that the two values are the same
+            Assert.AreEqual(ATariff.Texts, tariffTexts);
+        }
+
+        [TestMethod]
+        public void TariffUpfrontOK()
+        {
+            //create an instance of the class
+            clsTariff ATariff = new clsTariff();
+            //create some test data 
+            double tariffUpfront = 90.00;
+            //assign the data to property
+            ATariff.Upfront = tariffUpfront;
+            //test to see that the two values are the same
+            Assert.AreEqual(ATariff.Upfront, tariffUpfront);
+        }
+        //End Of Connor's Tests
+    }
 }
-//End Of Connor's Tests
