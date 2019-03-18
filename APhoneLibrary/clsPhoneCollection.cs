71c67defd75bc7bb7c48d41fba859ded9a02b91a
P17181674 <P17181674@GWHXX653WIN008W>
2019-03-18 15:12:29 +0000

    added this phone and count properties and there test methods

--- APhoneLibrary/clsPhoneCollection.cs
+++ APhoneLibrary/clsPhoneCollection.cs
@@ -5,5 +5,7 @@ namespace APhoneLibrary
     public class clsPhoneCollection
     {
         public List<clsPhone> PhoneList { get; set; }
+        public int Count { get; set; }
+        public clsPhone ThisPhone { get; set; }
     }
 }
--- APhoneTestProject/tstPhoneCollection.cs
+++ APhoneTestProject/tstPhoneCollection.cs
@@ -43,5 +43,39 @@ namespace APhoneTestProject
             //test to see that the two values are the same
             Assert.AreEqual(AllPhones.PhoneList, TestList);
         }
+
+        [TestMethod]
+        public void CountPropertyOk()
+        {
+            //create an instance of the class we want ot create
+            clsPhoneCollection AllPhones = new clsPhoneCollection();
+            //create some test data to assign to the property
+            Int32 SomeCount = 0;
+            //assign the data to the property
+            AllPhones.Count = SomeCount;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllPhones.Count, SomeCount);
+        }
+
+        [TestMethod]
+        public void ThisPhonePropertyOk()
+        {
+            //create an instance of the class we want ot create
+            clsPhoneCollection AllPhones = new clsPhoneCollection();
+            //create some test data to assign to the property
+            clsPhone TestPhone = new clsPhone();
+            //set the properties to the test object
+            TestPhone.PhoneId = 1;
+            TestPhone.Make = "Apple";
+            TestPhone.Model = "IPhone X";
+            TestPhone.PhoneNo = "07749493975";
+            TestPhone.Price = "500";
+            TestPhone.ScreenSize = "7.2";
+            TestPhone.CameraQuality = "FHD";
+            //assign the data to the property
+            AllPhones.ThisPhone = TestPhone;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllPhones.ThisPhone, TestPhone);
+        }
     }
 }
