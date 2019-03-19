523d4b04932b1dd4c82d2c31c91f529761d5d41a
P17181674 <P17181674@GWHXX653WIN008W>
2019-03-19 14:47:24 +0000

    got phones to display and began the add method for testing

--- APhoneFrontEnd/Default.aspx.cs
+++ APhoneFrontEnd/Default.aspx.cs
@@ -12,8 +12,28 @@ namespace APhoneFrontEnd
         //this function handles the load event for the page
         protected void Page_Load(object sender, EventArgs e)
         {
+            //if this is the first time the page is displayed 
+            if(IsPostBack == false)
 
-
+        {
+                //update the list box
+                DisplayPhones();
+            }
         }
+
+        void DisplayPhones()
+        {
+            //create an instance of the phone collection
+            APhoneLibrary.clsPhoneCollection Phones = new APhoneLibrary.clsPhoneCollection();
+            //set the data source to the list of phones in the collection
+            lstPhones.DataSource = Phones.PhoneList;
+            //set the name of the primary key
+            lstPhones.DataValueField = "PhoneId";
+            //set the data field to display
+            lstPhones.DataTextField = "Model";
+            //bind the data to the list
+            lstPhones.DataBind();
+        }
+
     }
 }
